4693ec98bd5daac12e18a7f2106adcbc0ee7166d
nayana1729 <109027318+nayana1729@users.noreply.github.com>
2025-07-05 17:59:42 -0700

    Update forms.py

--- polls/forms.py
+++ polls/forms.py
@@ -1,9 +1,4 @@
 from django import forms
-
-class BMIForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
-    height = forms.FloatField(label = 'Height in metres')
-    weight = forms.FloatField(label = 'Weight in kgs')
     
 class SearchForm(forms.Form):
     search = forms.CharField (label='Search', max_length = 100)
